6e8a1a37813fdca308d00aa26d97abd29c1a1c40
AnaAguileraMorales88 <anitaaguilera@hotmail.com>
2026-01-23 11:45:34 +0100

    Más ejercicios

--- ejercicio1/ejercicio1.py
+++ ejercicio1/ejercicio1.py
@@ -39,3 +39,66 @@ print(a / b) #Una / Devuelve siempre un número decimal, aunque la división sea
 a = 10
 b = 3
 print(a // b) #Dos // Devuelve solo la parte entera del resultado (sin decimales).
+
+# Truco mental rápido
+# / → piensa en matemáticas normales
+# // → piensa en “suelo” (floor)
+# % → piensa en “lo que falta para llegar al siguiente múltiplo”
+
+edad = 20
+tiene_carnet = True
+
+print(edad >= 18 and tiene_carnet)
+
+print ((2 ** 4), (2 * 4.), (2 *4))
+
+ALICIA = "coches" 
+Alicia = "colores"
+alicia = "frutas"
+
+print(Alicia) #Imprime el que está escrito igual
+
+var = 1
+nombre_cliente = "Ana Aguilera"
+cuenta_bancaria = 1000.0
+
+print(var, nombre_cliente, cuenta_bancaria)
+
+var = "3.8.5"
+print("Python version: " + var + "\n" "otra linea")
+
+# Imagina que estem fent una recepta per a fer galetes.
+# Suposem que tens 2 galetes i el teu amic et dona 3 més. 
+# Llavors, quantes galetes tens en total?
+# Codi: 
+
+galetes = 2
+galetes += 3
+print("Tinc", galetes, "galetes") # Para ahorrar código sería galetes 2 + 3 directamente
+
+
+# Imagina que tens 5 galetes i decideixes menjar-te 2. 
+# Quantes galetes et queden?
+# Codi:
+
+galetes = 5
+galetes -= 2
+print(galetes)
+
+
+# Ara, imagina que vols fer 3 caixes de galetes, i en cada caixa poses 4 galetes. 
+# Quantes galetes tens en total? 
+# Codi:
+
+caixes = 3
+galetes_caixes = 4
+total_galetes = caixes * galetes_caixes
+print(total_galetes) 
+
+# I si tens 12 galetes i les vols compartir entre 4 amics, quantes galetes li toquen a cadascun?
+# Codi:
+
+galetes = 12
+amics = 4
+galetas_amics = galetes // amics
+print(galetas_amics)
