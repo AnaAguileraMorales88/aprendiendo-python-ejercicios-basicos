74849c60dcf292593b1f6e6af48dbde09b274270
AnaAguileraMorales88 <anitaaguilera@hotmail.com>
2026-01-30 12:47:20 +0100

    Más ejercicios(condicionales)

---
+++ ejercicio5.py
@@ -0,0 +1,15 @@
+""" Escriu un programa de dues línies que prengui un número com a 
+entrada i imprimeixi True si el número és major que 10, i False si no ho és."""
+
+a = int(input(""))
+print(a > 10)
+
+# Utilitzant un dels operadors de comparació en Python, escriu un programa 
+# simple de dues línies que prengui el paràmetre n com a entrada, que és un enter, i 
+
+# imprimeix False si n és menor que 100, i True si n és major o igual que 100.
+
+# No has de crear cap bloc if (parlarem d'ells molt aviat). Prova el teu codi utilitzant les dades que et proporcionem.
+
+n = int(input())
+print(n >= 100)
