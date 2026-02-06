515fe73e9d7fe670453b066a5fda220e0d18cfbd
AnaAguileraMorales88 <anitaaguilera@hotmail.com>
2026-02-06 13:12:46 +0100

    Ejercicio añadido de bucle for

---
+++ Bucles/for/ejercicio1.py
@@ -0,0 +1,15 @@
+# Escenario
+# ¿Sabes lo que es Mississippi? Bueno, es el nombre de uno de los estados y ríos de Estados Unidos. 
+# El río Mississippi tiene aproximadamente 2,340 millas de largo, lo que lo convierte en el segundo río más largo de Estados Unidos (el más largo es el río Missouri). 
+# ¡Es tan largo que una sola gota de agua tarda 90 días en recorrer toda su longitud!
+# La palabra Mississippi también se usa con un propósito ligeramente diferente: para contar "mississippily" (a la manera de Mississippi).
+# Si no estás familiarizado con la frase, estamos aquí para explicarte lo que significa: se utiliza para contar los segundos.
+# La idea detrás de esto es que agregar la palabra Mississippi a un número al contar los segundos en voz alta hace que suene más cercano al reloj, y por lo tanto "uno Mississippi, dos Mississippi, tres Mississippi" tomará aproximadamente tres segundos reales. 
+# A menudo lo usan los niños que juegan a las escondidas para asegurarse de que quien cuenta lo haga de manera justa.
+# Tu tarea aquí es muy simple: escribe un programa que use un bucle for para "contar al estilo Mississippi" hasta cinco. 
+# Habiendo contado hasta cinco, el programa debe imprimir en pantalla el mensaje final "¡Listos o no, allá voy!"
+
+for i in range(1, 6):
+    print(i , "Mississippi")
+
+print("¡Listos o no, allá voy!")
