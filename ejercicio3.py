09c70f24638d3d9df9c69b0d567ccc63b506fb5e
Ana Aguilera <anitaaguilera@hotmail.com>
2026-02-13 12:45:59 +0100

    Más ejercicios

---
+++ ejercicio3.py
@@ -0,0 +1,8 @@
+# 1. Crear una llista d'animals
+animales = ["león","tigre","perro","mono","elefante"]
+
+# 2. Utilitzar un bucle for per recórrer la llista
+for animal in animales:
+        print("Me gustan los", animal)
+
+    # 3. Imprimir un missatge divertit per a cada animal
---
+++ ejercicio4.py
@@ -0,0 +1,9 @@
+numeros = [1, 2, 3]
+numeros.append(4) #Sirve para añadir un elemento al FINAL de la lista. Siempre al final
+
+print(numeros)
+
+numeros = [1, 2, 3]
+numeros.insert(1, 99) #Sirve para insertar un elemento en una posición específica.En la posición que tú elijas
+
+print(numeros)
